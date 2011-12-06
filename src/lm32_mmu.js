05a13aff1d9d32cc9323b924c3b713043f08ba4f
Reginaldo Silva <reginaldo@ubercomp.com>
2011-12-06 02:39:50 -0200

    Dummy handlers for invalid address

--- src/lm32_mmu.js
+++ src/lm32_mmu.js
@@ -14,6 +14,15 @@
 lm32.MMU = function() {
     this.handlers = [];
     this.last_handler = undefined;
+    var dummy = function() { return 0 };
+    this.dummy_handler = {
+            read_8: dummy,
+            read_16: dummy,
+            read_32: dummy,
+            write_8: dummy,
+            write_16: dummy,
+            write_32: dummy
+    };
 };
 
 lm32.MMU.prototype.get_handler_for = function(addr, name) {
@@ -32,7 +41,8 @@ lm32.MMU.prototype.get_handler_for = function(addr, name) {
             this.last_handler = handler;
         }
     }
-    if(!handler) {
+    if(handler === undefined) {
+        handler = this.dummy_handler;
         lm32.util.trace("MMU get_handler_for (called by " + name + "): no handler found for address 0x" + addr.toString(16));
     }
     return handler;
