0c260c13b387815bf4359e81acc74aa9b3ddd6a6
Reginaldo Silva <reginaldo.silva@touchtec.com.br>
2011-12-08 18:35:07 -0200

    LM32 PIC Controller

--- src/lm32_pic.js
+++ src/lm32_pic.js
@@ -1,5 +1,66 @@
 /**
+ * Javascript PIC Controller;
  * Copyright (c) 2011 Reginaldo Silva (reginaldo@ubercomp.com)
  * Created: 08/12/11 02:24
  */
 "use strict";
+lm32.Lm32Pic = function(raise_irq, lower_irq) {
+    var self = this;
+    self.im = 0;
+    self.ip = 0;
+    self.irq_state = 0;
+    
+    function dump() {
+        return ("im=" + self.im + " ip=" + self.ip + ' irq_state=' + self.irq_state);
+    }
+    self.dump = dump;
+
+    function update_irq() {
+        self.ip |= self.irq_state;
+
+        if (self.ip & self.im) {
+            raise_irq();
+        } else {
+            lower_irq();
+        }
+    }
+
+    function irq_handler (irq, level) {
+        switch(level) {
+            case 0:
+                self.irq_state &= ~(1 << irq);
+                break;
+            case 1:
+                self.irq_state |= (1 << irq);
+                break;
+            default:
+                throw 'Invalid IRQ';
+                break;
+        }
+        update_irq();
+    }
+    self.irq_handler = irq_handler;
+
+    function set_im(im) {
+        self.im = im;
+        update_irq();
+    }
+    self.set_im = set_im;
+
+    function set_ip(ip) {
+        /* ack interrupt */
+        self.ip &= ~ip;
+        update_irq();
+    }
+    self.set_ip = set_ip;
+
+    function get_im() {
+        return self.im;
+    }
+    self.get_im = get_im;
+
+    function get_ip() {
+        return self.ip;
+    }
+    self.get_ip = get_ip;
+};
