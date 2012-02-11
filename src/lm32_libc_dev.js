521deec40a58e39dffafb510fa4176f971696c08
Reginaldo Silva <reginaldo@ubercomp.com>
2012-02-11 16:26:48 -0200

    Adding skeletons for all string.h functions used by the Linux Kernel.

--- src/lm32_libc_dev.js
+++ src/lm32_libc_dev.js
@@ -1,20 +1,77 @@
 /**
+ * Javascript-side implementation of some libc functions
+ * for speed.
+ *
  * Copyright (c) 2012 Reginaldo Silva (reginaldo@ubercomp.com)
  * Created: 03/02/12
  */
-lm32.memops_dev = function(mmu, ram_array, ram_base, ram_size) {
+lm32.libc_dev = function(mmu, ram_array, ram_base, ram_size) {
+    var u32 = lm32.bits.unsigned32;
 
-    var R_TO = 0;    // memcpy destination addr, memset destination addr
-    var R_FROM = 1;  // memcpy origin addr
-    var R_SIZE = 2;  // memcpy size, memset size
-    var R_COPY = 3;  // command to copy
-    var R_VALUE = 4; // memset value
-    var R_SET = 5;
-    var R_MAX = 6;
+    var NULL_VALUE = 0;
+
+    var R_ARG0  = 0;
+    var R_ARG1  = 1;
+    var R_ARG2  = 2;
+    var R_ARG3  = 3;
+    var R_ARG4  = 4;
+    var R_ARG5  = 5;
+    var R_ARG6  = 6;
+    var R_ARG7  = 7;
+    var R_ARG8  = 8;
+    var R_ARG9  = 9;
+    var R_ARG10 = 10;
+    var R_ARG11 = 11;
+    var R_ARG12 = 12;
+    var R_ARG13 = 13;
+    var R_FN    = 14; // the function being executed
+    var R_RET   = 15; // the return value (usually an address)
+
+    var R_MAX  = 16;
+
+    // naming convention: FN_HEADER_FUNCTION
+
+
 
     var RESET_VALUE = -1;
 
+    var FN_STRING_MEMCHR      = 0;
+    var FN_STRING_MEMCMP      = 1;
+    var FN_STRING_MEMCPY      = 2;
+    var FN_STRING_MEMMOVE     = 3;
+    var FN_STRING_MEMSCAN     = 4;
+    var FN_STRING_MEMSET      = 5;
+    var FN_STRING_STRCASECMP  = 6;
+    var FN_STRING_STRCAT      = 7;
+    var FN_STRING_STRCHR      = 8;
+    var FN_STRING_STRCMP      = 9;
+    var FN_STRING_STRCPY      = 10;
+    var FN_STRING_STRCSPN     = 11;
+    var FN_STRING_STRLCAT     = 12;
+    var FN_STRING_STRLCPY     = 13;
+    var FN_STRING_STRLEN      = 14;
+    var FN_STRING_STRNCASECMP = 15;
+    var FN_STRING_STRNCAT     = 16;
+    var FN_STRING_STRNCHR     = 17;
+    var FN_STRING_STRNCMP     = 18;
+    var FN_STRING_STRNCPY     = 19;
+    var FN_STRING_STRNICMP    = 20;
+    var FN_STRING_STRNLEN     = 21;
+    var FN_STRING_STRPBRK     = 22;
+    var FN_STRING_STRRCHR     = 23;
+    var FN_STRING_STRSEP      = 24;
+    var FN_STRING_STRSPN      = 25;
+    var FN_STRING_STRSTR      = 26;
+    var FN_STRING_STRSTRIP    = 27;
+
     var regs = new Array(R_MAX);
+
+    // instruction implementation declarations:
+    var impls = new Array();
+    impls[FN_STRING_MEMCPY] = string_memcpy;
+    impls[FN_STRING_MEMMOVE] = string_memmove;
+    impls[FN_STRING_MEMSET] = string_memset;
+
 
     function reset() {
         for(var i = 0; i < R_MAX; i++) {
@@ -30,101 +87,140 @@ lm32.memops_dev = function(mmu, ram_array, ram_base, ram_size) {
         return (addr >= ram_base) && (addr < ram_base + ram_size);
     }
 
+    // String funcitons
 
-    function do_copy_ram() {
-        var to_orig = regs[R_TO] - ram_base;
-        var from_orig = regs[R_FROM] - ram_base;
-        var size = regs[R_SIZE];
-        for(var i = 0; i < size; i++) {
-            ram_array[to_orig + i] = ram_array[from_orig + i];
-        }
-    }
+    function string_memchr() {}
 
-    function do_copy_mmu() {
-        var to_orig = regs[R_TO];
-        var from_orig = regs[R_FROM];
-        var size = regs[R_SIZE];
-        for(var i = 0; i < size; i++) {
-            mmu.write_8(to_orig + i, mmu.read_8(from_orig + i));
-        }
-    }
+    function string_memcmp() { }
 
-    function do_copy() {
-        // JAVASCRIPT memcpy implementation
-        if(regs[R_TO] === RESET_VALUE ||
-            regs[R_FROM] === RESET_VALUE ||
-            regs[R_SIZE] === RESET_VALUE) {
-            throw "memcpy_dev: Not ready to copy!!"
-        }
-
-        if(regs[R_SIZE] === 0) {
-            return;
-        }
-
-        //console.log('memcpy from = ' + lm32.bits.format(regs[R_FROM]) +
-        // ' to =' + lm32.bits.format(regs[R_TO]) +
-        // ' size =' + lm32.bits.format(regs[R_SIZE]));
-
-        if(region_from_ram(regs[R_TO], regs[R_SIZE]) &&
-            region_from_ram(regs[R_FROM], regs[R_SIZE])) {
-            do_copy_ram();
+    function string_memcpy(to, from, size) {
+        to = u32(to);
+        from = u32(from);
+        size = u32(size);
+        if(size === 0) { return; }
+        var i;
+        if(region_from_ram(to, size) && region_from_ram(from, size)) {
+            to -= ram_base;
+            from -= ram_base;
+            for(i = 0; i < size; i++) {
+                ram_array[to + i] = ram_array[from + i];
+            }
         } else {
-            do_copy_mmu();
+            for(i = 0; i < size; i++) {
+                mmu.write_8(to + i, mmu.read_8(from + i));
+            }
         }
-        reset();
-
     }
 
-    function do_set_ram() {
-
-    }
-
-    function do_set_mmu() {
-
-    }
-
-    function do_set() {
-        // JAVASCRIPT MEMSET IMPLEMENTATION
-
-
-        if(region_from_ram(regs[R_TO], regs[R_SIZE])) {
-            do_set_ram();
+    function string_memmove(dest, src, count) {
+        dest = u32(dest);
+        src = u32(src);
+        count = u32(count);
+        if(count === 0) { return; }
+        // TODO support outside ram:
+        // TODO optimize
+        if(region_from_ram(src, count) && region_from_ram(dest, count)) {
+            var tmp;
+            var s;
+            dest -= ram_base;
+            src -= ram_base;
+            if(dest <= src) {
+                tmp = dest;
+                s = src;
+                while(count--) {
+                    ram_array[tmp++] = ram_array[s++];
+                }
+            } else {
+                tmp = dest + count;
+                s = src + count;
+                while(count--) {
+                    ram_array[tmp--] = ram_array[s--];
+                }
+            }
         } else {
-            do_set_mmu();
+            throw ("libc_dev: memmove is not supported outside ram");
         }
-
     }
 
+    function string_memscan() { }
+
+    function string_memset(s, c, count) {
+        s = u32(s);
+        c = u32(c);
+        count = u32(count);
+        if(count === 0) { return; }
+        if(region_from_ram(s, count)) {
+            s -= ram_base;
+            for(var i = 0; i < count; i++) {
+                ram_array[s + i] = c & 0xff;
+            }
+        } else {
+           for(var i = 0; i < count; i++) {
+               mmu.write_8(s + i, c);
+           }
+        }
+    }
+
+    function string_strcasecmp() {}
+    function string_strcat() {}
+    function string_strchr() {}
+    function string_strcmp() {}
+    function string_strcpy() {}
+    function string_strcspn() {}
+    function string_strlcat() {}
+    function string_strlcpy() {}
+    function string_strlen() {}
+    function string_strncasecmp() {}
+    function string_strncat() {}
+    function string_strnchr() {}
+    function string_strncmp() {}
+    function string_strncpy() {}
+    function string_strnicmp() {}
+    function string_strnlen() {}
+    function string_strpbrk() {}
+    function string_strrchr() {}
+    function string_strsep() {}
+    function string_strspn() {}
+    function string_strstr() {}
+    function string_strstrip() {}
+
+    // device handler functions
     function read_32(addr) {
         addr >>= 2;
         if (addr < 0 || addr >= R_MAX) {
-            throw ("memcpy_dev: Unknown address: " + addr);
+            throw ("libc_dev: Unknown address: " + addr);
         }
         return regs[addr];
     }
 
     function write_32(addr, value) {
         addr >= 2;
+        if(addr < 0 || addr >= R_MAX) {
+            throw ("libc_dev: Unknown address: " + addr);
+        }
+
         switch(addr) {
-            case R_TO:
-            case R_FROM:
-                regs[addr] = lm32.bits.unsigned32(value);
-                break;
-
-            case R_SIZE:
-            case R_VALUE:
-                regs[addr] = value;
-                break;
-
-            case R_COPY:
-                do_copy();
-                break;
-
-            case R_SET:
-                do_set();
+            case R_FN:
+                var fn = impls[value];
+                if(fn) {
+                    regs[R_RET] = RESET_VALUE;
+                    // TODO if a function with more than 4 args
+                    // TODO is implemented, change here
+                    var to_return = (fn)(regs[R_ARG0], regs[R_ARG1], regs[R_ARG2], regs[R_ARG3]);
+                    if(to_return !== undefined) {
+                        regs[R_RET] = to_return | 0;
+                    }
+                    // reset everything except ret
+                    var old_ret = regs[R_RET];
+                    reset();
+                    regs[R_RET] = old_ret;
+                } else {
+                    throw ("libc_dev: Function not implemented: " + value);
+                }
                 break;
             default:
-                throw "memcpy_dev: No such register: " + addr;
+                regs[addr] = value;
+                break;
         }
     }
 
@@ -135,6 +231,8 @@ lm32.memops_dev = function(mmu, ram_array, ram_base, ram_size) {
         };
         return handlers;
     }
+
+
 
     return {
         iomem_size: 4 * R_MAX,
